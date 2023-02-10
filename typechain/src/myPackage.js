4dbcfbb1d72f0aaf85c223aa997e54f8591d35fa
Shape Lee <shape1026@gmail.com>
2023-02-10 11:20:09 +0900

    return 수정

--- typechain/src/myPackage.js
+++ typechain/src/myPackage.js
@@ -4,12 +4,17 @@
  * @param {Object} config 
  * @param {boolean} config.debug
  * @param {string} config.url
- * @returns {boolean}
+ * @returns boolean
  */
 export function init(config) {
   return true;
 }
 
+/**
+ * Exits the program
+ * @param {number} code 
+ * @returns number
+ */
 export function exit(code) {
   return code + 1
 }
