796f8ebec22c69b8a65d46cbea0fce1366632b64
Shape Lee <shape1026@gmail.com>
2023-02-10 11:16:00 +0900

    allowJs:true 추가

--- typechain/src/myPackage.js
+++ typechain/src/myPackage.js
@@ -1,6 +1,15 @@
+// @ts-check
+/**
+ * initalizes the project
+ * @param {Object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
 export function init(config) {
   return true;
 }
+
 export function exit(code) {
   return code + 1
 }
