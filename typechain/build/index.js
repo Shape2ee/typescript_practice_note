a5265a4192aa9feb897eb2b05cd17a8d2b313ade
Shape Lee <shape1026@gmail.com>
2023-02-16 15:01:23 +0900

    예제 연습

--- typechain/build/index.js
+++ typechain/build/index.js
@@ -1,3 +1,37 @@
-const hello = () => {
-    return 'hello';
-};
+import crypto from 'crypto';
+class Block {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
+        this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
+    }
+}
+class Blockchain {
+    constructor() {
+        this.blocks = [];
+    }
+    getPrevHash() {
+        if (this.blocks.length === 0) {
+            return "";
+        }
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    addBlock(data) {
+        const newblock = new Block(this.getPrevHash(), this.blocks.length - 1, data);
+        this.blocks.push(newblock);
+    }
+    getBlocks() {
+        return [...this.blocks];
+    }
+}
+const blockchain = new Blockchain();
+blockchain.addBlock("first one");
+blockchain.addBlock("second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+console.log(blockchain.getBlocks());
